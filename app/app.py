a5a3b46fac36f367751fd349d3b604558d656f4f
Rushy Panchal <panchal.rushy@gmail.com>
2015-11-13 22:35:00 -0500

    Start basic web app
    The app folder contains most of the work. The folder structure is as follows
    
    app/ - main folder
    	static/ - static assets (CSS, JS, images, etc.)
    	views/ - view pages

---
+++ app/app.py
@@ -0,0 +1,36 @@
+# Rushy Panchal
+# HackPrinceton 2015
+# app/app.py
+
+from flask import Flask, render_template
+
+def main():
+	'''Create the server and start it'''
+	server = Server("config.FlaskSettings", template_folder = "views", static_folder = "static", static_url_path = "/static")
+	server.configureRoutes()
+	server.run()
+
+class Server(Flask):
+	'''Creates a basic Flask Web Server'''
+	def __init__(self, configPath, *args, **kwargs):
+		super(Server, self).__init__("HackPrinceton", *args, **kwargs)
+		self.config.from_object(configPath)
+
+	def shutdown(self): # not used as of now, need OS signal handling to do this
+		'''Shuts down the Flask server'''
+		with self.test_request_context():
+			shutdownFunc = flask.request.environ.get("werkzeug.server.shutdown")
+			if not shutdownFunc:
+				raise ValueError("Not running a Werkzeug server")
+			return shutdownFunc()
+
+	def configureRoutes(self):
+		'''Configure the routes for the server'''
+		@self.route("/index/")
+		@self.route("/")
+		def index():
+			'''GET the index page'''
+			return render_template("index.html")
+
+if __name__ == '__main__':
+	main()
---
+++ app/config.py
@@ -0,0 +1,11 @@
+# Rushy Panchal
+# HackPrinceton 2015
+# app/config.py
+
+class FlaskSettings(object):
+	'''Settings for Flask server'''
+	DEBUG = True
+	SECRET_KEY = "NtaTRKcZKw256RNMwp25Xqvw"
+
+	SERVER_NAME = "localhost:8080"
+	PREFERRED_URL_SCHEME = "http://"
