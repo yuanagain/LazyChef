b26a62a287ca3440e4578855ea6a06a51b9faf18
yuanagain <yuanw@princeton.edu>
2015-11-14 23:00:48 -0500

    documented KRAKEN

--- do_it_to_it.py
+++ do_it_to_it.py
@@ -1,10 +1,64 @@
+"""
+This module provides classes for loading a library of recipes.
+CONTENTS:
+============================================================
+I. CLASSES
+II. USEFUL METHODS
+III. EXAMPLE USAGE:
+============================================================
+============================================================
+I. CLASSES
+============================================================
+class KRAKEN():
+
+Description:
+Allows user to generate task orderings
+============================================================
+II. USEFUL METHODS
+============================================================
+    
+__init__(self, recipe_src = './test_recipes/'):
+    Description:
+    ------
+    Creates a KRAKEN object
+    
+    Parameters
+    ------
+    def_list_dir : fname (default =  ./recipe_files/')
+        A list of recipe defintions
+
+
+produce_dict(self, target):
+
+	Description:
+	------
+	Returns dict of tasks corresponding to target
+
+	Parameters:
+	------
+	target : list
+		A list of recipes we desire to complete
+
+============================================================
+III. EXAMPLE USAGE:
+============================================================
+from do_it_to_it import KRAKEN
+fg = KRAKEN()
+tg = ['Boil Water', 'Pasta']
+print(fg.produce_dict(tg))
+
+
+Author: Yuan Wang
+Contact/Support: yuanw@princeton.edu
+"""
+
 import brute_force as bf
 import taskNodeGeneratorUtils
 import todoListGenerator as tdlg
 from taskNodeGeneratorUtils import recipeLibrary
 
 
-class fileGen:
+class KRAKEN:
 	def __init__(self, recipe_src = './test_recipes/'):
 		self.rlibs = recipeLibrary(recipe_src)
 
@@ -19,7 +73,14 @@ class fileGen:
 
 	def produce_dict(self, target):
 		"""
-		returns dict of tasks corresponding to target
+		Description:
+		------
+		Returns dict of tasks corresponding to target
+
+		Parameters:
+		------
+		target : list
+			A list of recipes we desire to complete
 		"""
 		tlist = self.rlibs.extract_list(target)
 		tasklist = bf.maximizer(tlist)
@@ -41,7 +102,7 @@ def main2():
 	tdlg.tnodelist_tojson(tasklist, out_fname = './example.json')
 
 def main():
-	fg = fileGen()
+	fg = KRAKEN()
 	tg = ['Boil Water', 'Pasta']
 	print(fg.produce_dict(tg))
 
